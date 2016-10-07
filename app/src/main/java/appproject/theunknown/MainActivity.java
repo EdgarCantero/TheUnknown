9169a5270627d2ee8dfcf4bf61e4a6967c5b0b20
Edgar Cantero <cantero098@gmail.com>
2016-10-07 13:57:26 -0700

    milestone 4 complete.currently broken with sensorListener.

--- app/src/main/java/appproject/theunknown/MainActivity.java
+++ app/src/main/java/appproject/theunknown/MainActivity.java
@@ -1,12 +1,32 @@
 package appproject.theunknown;
 
 import android.support.v7.app.AppCompatActivity;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView answerText;
+
+    private SensorManager sensorManager;
+    private float acceleration;
+    private float currentAcceleration;
+    private float previousAcceleration;
+
+    private final SensorEventListener sensorListener = new SensorEventListener() {
+      @Override
+      public void onSensorChanged(SensorEvent event) {
+
+      }
+
+      @Override
+      public void onAccuracyChanged(Sensor sensor, int accuracy)  {
+
+
+      }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
