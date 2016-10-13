a1608b51310672a17d804cf9cd5ae4a45599c82f
Edgar Cantero <cantero098@gmail.com>
2016-10-13 13:59:25 -0700

    Final commit

--- app/src/main/java/appproject/theunknown/MainActivity.java
+++ app/src/main/java/appproject/theunknown/MainActivity.java
@@ -1,16 +1,23 @@
 package appproject.theunknown;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.support.v7.app.AppCompatActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView answerText;
 
     private SensorManager sensorManager;
+    private Sensor accelerometer;
+
     private float acceleration;
     private float currentAcceleration;
     private float previousAcceleration;
@@ -18,6 +25,19 @@ public class MainActivity extends AppCompatActivity {
     private final SensorEventListener sensorListener = new SensorEventListener() {
       @Override
       public void onSensorChanged(SensorEvent event) {
+          float x = event.values[0];
+          float y = event.values[1];
+          float z = event.values[2];
+
+          previousAcceleration = currentAcceleration;
+          currentAcceleration = FloatMath.sqrt(x * x + y * y + z * z);
+          float delta = currentAcceleration = previousAcceleration;
+          acceleration = acceleration * 0.9f + delta;
+
+          if(acceleration > 15) {
+              Toast toast = Toast.makeText(getApplication(), "Device has shaken", Toast.LENGTH_SHORT);
+              toast.show();
+          }
 
       }
 
@@ -33,8 +53,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        acceleration = 0.0f;
+        currentAcceleration = SensorManager.GRAVITY_EARTH;
+        previousAcceleration = SensorManager.GRAVITY_EARTH;
+
         answerText = (TextView) findViewById(R.id.answerText);
         answerText.setText(Predictions.get().getPrediction());
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(sensorListener);
     }
 }
